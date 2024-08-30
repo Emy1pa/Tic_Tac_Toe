942b6b3f43d07c5084263688a20c9c5722d2887d
imane souaouti <souaouti.iman@gmail.com>
2024-08-30 11:27:05 +0100

    add title, buttons of x and o, and also restart button, plus some image styling

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 const container = document.getElementById("container");
+
+// Create cell
 for (let i = 0; i < 400; i++) {
   const cell = document.createElement("div");
   cell.classList.add("cell");
   container.appendChild(cell);
 }
+
+// create button container
+const buttonContainer = document.createElement("div");
+buttonContainer.className = "button-container";
+container.appendChild(buttonContainer);
